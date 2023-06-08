c8df11b959de0baa6f917016bed1644e39038b2f
Isadora Pilau <isadorapda@gmail.com>
2023-06-08 14:11:16 +0100

    test: 💍 add test for fetching list of a question's answers

---
+++ src/domain/forum/application/services/fetch-question-answers.spec.ts
@@ -0,0 +1,64 @@
+import { InMemoryAnswersRopository } from 'tests/repositories/in-memory-answers-repository'
+import { FetchListQuestionAnswersService } from './fetch-question-answers'
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+import { createAnswer } from 'tests/factories/create-answer'
+
+let answersRepository: InMemoryAnswersRopository
+let sut: FetchListQuestionAnswersService
+
+describe("Fetch Questions' Answers Service", () => {
+  beforeEach(() => {
+    answersRepository = new InMemoryAnswersRopository()
+    sut = new FetchListQuestionAnswersService(answersRepository)
+  })
+
+  test("Should be able to fetch list of a question's answers ", async () => {
+    await answersRepository.create(
+      createAnswer({ questionId: new UniqueEntityID('question-1') }),
+    )
+    await answersRepository.create(
+      createAnswer({ questionId: new UniqueEntityID('question-1') }),
+    )
+
+    const { answers } = await sut.fetchListAnwersService({
+      questionId: 'question-1',
+      pagination: { page: 1, limit: 10 },
+    })
+
+    expect(answers).toHaveLength(2)
+    expect(answers).toEqual([
+      expect.objectContaining({
+        questionId: new UniqueEntityID('question-1'),
+      }),
+      expect.objectContaining({
+        questionId: new UniqueEntityID('question-1'),
+      }),
+    ])
+  })
+
+  test("Should be able to fetch paginated list of a question's answers", async () => {
+    for (let i = 1; i <= 12; i++) {
+      await answersRepository.create(
+        createAnswer(
+          { questionId: new UniqueEntityID('question-1') },
+          new UniqueEntityID(`answer-${i}`),
+        ),
+      )
+    }
+
+    const { answers } = await sut.fetchListAnwersService({
+      questionId: 'question-1',
+      pagination: { page: 2, limit: 10 },
+    })
+
+    expect(answers).toHaveLength(2)
+    expect(answers).toEqual([
+      expect.objectContaining({
+        id: new UniqueEntityID(`answer-11`),
+      }),
+      expect.objectContaining({
+        id: new UniqueEntityID(`answer-12`),
+      }),
+    ])
+  })
+})
